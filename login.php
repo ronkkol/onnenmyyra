1b2b2dbd4263fb8c7aee594d4965c58412b28e9e
Defelot <defelot@gmail.com>
2012-10-25 16:15:55 +0300

    session_regenerate_id funktio korjattu

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-session_regenerate_id([ $delete_old_session = true ]);
+session_regenerate_id(true);
  ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
    "http://www.w3.org/TR/html4/loose.dtd">
